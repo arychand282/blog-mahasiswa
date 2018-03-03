deb8f1559d26109f7899ac1ddd07298966cc3073
Bagus Ari Chandra <b.chandra@teravintech.com>
2018-03-03 22:50:23 +0700

    try post form

--- src/main/java/com/projectside/blogmahasiswa/controller/web/WelcomeController.java
+++ src/main/java/com/projectside/blogmahasiswa/controller/web/WelcomeController.java
@@ -1,13 +1,23 @@
 package com.projectside.blogmahasiswa.controller.web;
 
+import com.projectside.blogmahasiswa.domain.Greeting;
+import com.projectside.blogmahasiswa.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.Map;
 
 @Controller
 public class WelcomeController {
+
+    @Autowired
+    private UserService userService;
 
     // inject via application.properties
     @Value("${welcome.message:test}")
@@ -17,6 +27,17 @@ public class WelcomeController {
     public String welcome(Map<String, Object> model) {
         model.put("message", this.message);
         return "welcome";
+    }
+
+    @GetMapping("/greeting")
+    public String greetingForm(Model model) {
+        model.addAttribute("greeting", new Greeting());
+        return "greeting";
+    }
+
+    @PostMapping("/greeting")
+    public String greetingSubmit(@ModelAttribute Greeting greeting) {
+        return "result";
     }
 
 }
---
+++ src/main/java/com/projectside/blogmahasiswa/domain/Greeting.java
@@ -0,0 +1,13 @@
+package com.projectside.blogmahasiswa.domain;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class Greeting {
+
+    private long id;
+    private String content;
+
+}
